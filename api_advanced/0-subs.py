a56e5e3042d542c866bd878f44be71486d06f16d
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-11-07 10:10:50 -0800

    task 0

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""0-subs.py"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Set a custom User-Agent to avoid "Too Many Requests" errors """
+    headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
+
+    """ Construct the URL for the subreddit """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    """ Send a GET request to the Reddit API """
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    """ Check if the request was successful (status code 200) """
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        # If the subreddit is not found or there's an issue with the request, return 0
+        return 0
+
+
