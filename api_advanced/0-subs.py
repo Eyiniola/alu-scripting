e796deda529bdf6e5c4cb78d184c6e5456b0437c
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-11-07 22:19:35 -0800

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""0-subs.py"""
+
+"""number of subscribers"""
 
 import requests
 
-"""Reddit subscribers"""
-
 def number_of_subscribers(subreddit):
-  """Reddit subscribers"""     
+       """subreddit subscribers"""
     headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
+
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
 
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
     else:
+
         return 0
 
