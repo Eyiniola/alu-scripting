41ca77942e8baca49bdd228dc27c3e32263e481d
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-11-08 00:50:20 -0800

    task 2

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
-"""2-recurse.py"""
-
-
+""" recursive function that queries the Reddit API
+and returns a list containing the titles of all hot
+articles for a given subreddit."""
 import requests
 
-def recurse(subreddit, hot_list=[], after=None):
+
+def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
-
-    # Set a custom User-Agent to avoid "Too Many Requests" errors
-    headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
-
-    # Construct the URL for the subreddit's hot posts with pagination
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-
-    # Include the 'after' parameter for pagination
-    if after:
-        url += f'?after={after}'
-
-    # Send a GET request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is not valid
-    if response.status_code != 200:
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "My-User-Agent"
+        }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
 
-    # Parse the JSON response
-    data = response.json()
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-    # Extract titles from the current page and add them to the hot_list
-    hot_list.extend(post['data']['title'] for post in data.get('data', {}).get('children', []))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
 
-    # Check if there are more pages (next page) and continue recursively
-    if data['data']['after']:
-        return recurse(subreddit, hot_list, data['data']['after'])
-    else:
-        return hot_list
 
