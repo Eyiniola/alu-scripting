245054747c7010197272e7a540f99fbea4d1d44b
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-11-08 01:08:58 -0800

    task 3

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,65 +1,61 @@
 #!/usr/bin/python3
-
-"""3-count.py"""
-
+"""Doc"""
 import requests
 
 
-def count_words(subreddit, word_list, counts={}, after=None):
-    """Query Reddit API, count keywords, and print results.
+def count_words(subreddit, word_list, after="", words_count={}):
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    res = requests.get(url, headers=header, params=param)
 
-    Args:
-        subreddit (str): The name of the subreddit to search.
-        word_list (list of str): List of keywords to count.
-        counts (dict, optional): Dictionary to store keyword counts.
-        after (str, optional): The 'after' parameter for pagination.
+    if res.status_code != 200:
+        return
 
-    Returns:
-        None
+    json_res = res.json()  # chch
+    after = json_res.get('data').get('after')
+    has_next = after is not None
+    hot_titles = []
+    words = [word.lower() for word in word_list]
 
-    Recursively queries the Reddit API for hot articles in the specified subreddit,
-    counts the occurrences of keywords from the word_list in the article titles,
-    and prints the results sorted in descending order by count and ascending order
-    alphabetically (if counts are the same). Words not found are skipped.
+    if len(words_count) == 0:
+        words_count = {word: 0 for word in words}
+    # print(words_count)
+    hot_articles = json_res.get('data').get('children')
+    [hot_titles.append(article.get('data').get('title'))
+     for article in hot_articles]
 
-    """
+    # loop through all titles
+    for i in range(len(hot_titles)):
+        # make the title as a list of word
+        # title_words = hot_titles[i].lower().split()
+        for title_word in hot_titles[i].lower().split():
+            for word in words:
+                if word.lower() == title_word:
+                    words_count[word] = words_count.get(word) + 1
+                # else:
+                #     # pass
+                #     print(word.lower() + " != " + title_word)
 
-
-    # Set a custom User-Agent to avoid "Too Many Requests" errors
-    headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
-
-    # Construct the URL for the subreddit's hot posts with pagination
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-
-    # Include the 'after' parameter for pagination
-    if after:
-        url += f'?after={after}'
-
-    # Send a GET request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is not valid
-    if response.status_code != 200:
-        return None
-
-    # Parse the JSON response
-    data = response.json()
-
-    # Extract titles from the current page and count keyword occurrences
-    for post in data.get('data', {}).get('children', []):
-        title = post['data']['title'].lower()
-        for word in word_list:
-            if word in title and not title.startswith(word + '.') and not title.startswith(
-                    word + '!') and not title.startswith(word + '_'):
-                counts[word] = counts.get(word, 0) + 1
-
-    # Check if there are more pages (next page) and continue recursively
-    if data['data']['after']:
-        return count_words(subreddit, word_list, counts, data['data']['after'])
+    if has_next:
+        # print(after + "\t" + str(has_next))
+        return count_words(subreddit, word_list, after, words_count)
     else:
-        # Print results in descending order by count and ascending order alphabetically
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_counts:
-            print(f"{word}: {count}")
 
+        words_count = dict(filter(lambda item: item[1] != 0,
+                                  words_count.items()))
+        # their python version is not making people’s life easier
+        # words_count = {key: value for key, value in
+        #                sorted(words_count.items(),
+        #                       key=lambda item: item[1], reverse=True)}
+
+        words_count = sorted(words_count.items(),
+                             key=lambda item: item[1],
+                             reverse=True)
+
+        for i in range(len(words_count)):
+            print("{}: {}".format(words_count[i][0],
+                                  words_count[i][1]))
 
