80772142a8e032a930b9ca4140132700c71daeb0
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-11-07 23:01:50 -0800

    task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,31 @@
-#!/usr/bin/python3
-"""getting the top ten hottest post"""
+#!/usr/bin/python3 
+"""top_ten_posts
 
+This module defines a function to query the Reddit API and print the titles of the first 10 hot posts for a given subreddit.
 
+"""
 
 import requests
 
 def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts for a given subreddit.
 
-    headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
+    Args:
+        subreddit (str): The name of the subreddit for which you want to retrieve the hot posts.
 
+    Returns:
+        None
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    Prints the titles of the first 10 hot posts for the specified subreddit. If the subreddit is not found or there's an issue with the request, it prints 'None'.
 
-
+    """
+     headers = {'User-Agent': 'YourUniqueUserAgent/1.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-
     if response.status_code == 200:
-        return response.json().get("data").get("top ten")
+        data = response.json()
+        for post in data.get('data', {}).get('children', []):
+            print(post.get('data', {}).get('title', 'Title not available'))
     else:
-        return None
+        print(None)
